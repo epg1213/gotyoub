ae0df115048470571cb5ed599d36bb6412fe5355
eliot <epg1213@gmail.com>
2023-03-09 00:30:15 +0100

    now recursively encrypting whole directories !

--- gotyoub.c
+++ gotyoub.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <dirent.h>
+#include <sys/stat.h>
 #define p 1009
 #define q 1109
 #define e 1019
@@ -49,72 +51,72 @@ long rsa(int key, int mod, long msg){
 }
 
 int getInt(char a, char b){
-    if(a>57){
-        a-=87;
-    } else {
-        a-=48;
-    }
-    if(b>57){
-        b-=87;
-    } else {
-        b-=48;
-    }
-    return 16*a+b;
+  if(a>57){
+    a-=87;
+  } else {
+    a-=48;
+  }
+  if(b>57){
+    b-=87;
+  } else {
+    b-=48;
+  }
+  return 16*a+b;
 }
 
 void byg_to_gyb(char* bygpath, char* gybpath){
-    srand(time(NULL));
-    FILE* rptr=fopen(bygpath, "r");
-    FILE* wptr=fopen(gybpath, "wb");
-    char a=' ';
-    char i=getc(rptr);
-    int random;
-    char c;
-    while(i!=EOF){
-        random=rand()%16;
-        if(random<5){
-            c=97+random;
-        } else {
-            c=i;
-            i=getc(rptr);
-        }
-        if(a==' '){
-            a=c;
-        } else {
-            putc(getInt(a, c), wptr);
-            a=' ';
-        }
+  srand(time(NULL));
+  FILE* rptr=fopen(bygpath, "r");
+  FILE* wptr=fopen(gybpath, "wb");
+  char a=' ';
+  char i=getc(rptr);
+  int random;
+  char c;
+  while(i!=EOF){
+    random=rand()%16;
+    if(random<5){
+      c=97+random;
+    } else {
+      c=i;
+      i=getc(rptr);
     }
-    if(a!=' '){
-        random=rand()%5;
-        c=97+random;
-        putc(getInt(a, c), wptr);
+    if(a==' '){
+      a=c;
+    } else {
+      putc(getInt(a, c), wptr);
+      a=' ';
     }
+  }
+  if(a!=' '){
+    random=rand()%5;
+    c=97+random;
+    putc(getInt(a, c), wptr);
+  }
 
-    fclose(rptr);
-    fclose(wptr);
+  fclose(rptr);
+  fclose(wptr);
 }
 
 void gyb_to_byg(char* gybpath, char* bygpath){
     
-    FILE* rptr=fopen(gybpath, "rb");
-    FILE* wptr=fopen(bygpath, "w");
+  FILE* rptr=fopen(gybpath, "rb");
+  FILE* wptr=fopen(bygpath, "w");
 
-    int i=getc(rptr);
-    while (i!=EOF){
-        char* c=malloc(2);
-        sprintf(c, "%02x", i);
-        if (c[0]<58 || 101<c[0] ){
-            putc(c[0], wptr);
-        }
-        if (c[1]<58 || 101<c[1] ){
-            putc(c[1], wptr);
-        }
-        i=getc(rptr);
-        free(c);
+  int i=getc(rptr);
+  while (i!=EOF){
+    char* c=malloc(2);
+    sprintf(c, "%02x", i);
+    if (c[0]<58 || 101<c[0] ){
+      putc(c[0], wptr);
     }
-    fclose(rptr);
-    fclose(wptr);
+    if (c[1]<58 || 101<c[1] ){
+      putc(c[1], wptr);
+    }
+    i=getc(rptr);
+    free(c);
+  }
+  fclose(rptr);
+  fclose(wptr);
 }
 
 /*
@@ -200,38 +202,75 @@ void decrypt(char * path){
 */
 int exists(const char *fname)
 {
-    FILE *file;
-    if ((file = fopen(fname, "r")))
-    {
-        fclose(file);
-        return 1;
-    }
-    return 0;
+  FILE *file;
+  if ((file = fopen(fname, "r")))
+  {
+    fclose(file);
+    return 1;
+  }
+  return 0;
 }
 
-void main(){
-  char path[100];
-  printf("please write the path to the file:\n");
-  scanf("%s", path);
-  char* ext = strrchr(path, '.');
-  if (!exists(path)) {
-    printf("error finding file: %s\n", path);
-    exit(1);
-  } else {
-    if(!ext){
-      printf("there is no extension\n");
-      ext=".NULL";
-    } else {
-      printf("extension is %s\n", ext + 1);
+int is_regular_file(const char *path)
+{
+  struct stat path_stat;
+  stat(path, &path_stat);
+  return S_ISREG(path_stat.st_mode);
+}
+
+void rec(const char *dname, void (*encdec)(char*))
+{
+  DIR *d;
+  struct dirent *dir;
+  d = opendir(dname);
+  if (d) {
+    while ((dir = readdir(d)) != NULL) {
+      if(strcmp(dir->d_name,".") && strcmp(dir->d_name,"..") && strcmp(dir->d_name,"gotyoub")){
+        char* fullpath=malloc(strlen(dname)+strlen(dir->d_name)+1);
+        strcpy(fullpath, dname);
+        strcat(fullpath, "/");
+        strcat(fullpath, dir->d_name);
+        if(is_regular_file(fullpath)){
+          printf("encrypting:%s\n", fullpath);
+          if (encdec==&decrypt){
+            fullpath[strlen(fullpath)-4]='\0';
+          }
+          encdec(fullpath);
+        } else {
+          printf(" entering :%s\n", fullpath);
+          rec(fullpath, encdec);
+          printf(" exiting  :%s\n", fullpath);
+        }
+        
+        free(fullpath);
+      }
     }
-    if(strcmp("gyb", ext+1)){
-      printf("encrypting: %s", path);
-      encrypt(path);
-    }else{
-      path[strlen(path)-4]='\0';
-      printf("decrypting: %s", path);
-      decrypt(path);
-    }
+    closedir(d);
   }
-  printf("\n");
+}
+
+void main(int argc, char* argv[]){
+  int dec=0;
+  if (argc==2 && (!strcmp(argv[1], "-e") || !strcmp(argv[1], "--encrypt"))){
+    printf("Encrypring mode enabled.\n");
+  } else if (argc==2 && (!strcmp(argv[1], "-d") || !strcmp(argv[1], "--decrypt"))){
+    printf("Decrypring mode enabled.\n");
+    dec=1;
+  } else {
+    printf("Usage:\ngotyoub [-e|--encrypt] :recursively encrypts a directory\ngotyoub [-d|--decrypt] :recursively decrypts a directory\n");
+    exit(0);
+  }
+  printf("Please enter the path to the direcrory:\ngyb!>");
+  char path[100]="NULL";
+  scanf("%s", path);
+  
+  while(path[0]!='/' || !exists(path) || is_regular_file(path)){
+    printf("Only valid absolute path to directories are allowed.\ngyb!>");
+    scanf("%s", path);
+  }
+  if (dec){
+    rec(path, &decrypt);
+  } else {
+    rec(path, &encrypt);
+  }
 }
